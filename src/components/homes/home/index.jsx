b140b4ce83fc830b5a7ff9ff094518828fff8538
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2024-01-16 01:51:57 +0500

    Update index.jsx

--- src/components/homes/home/index.jsx
+++ src/components/homes/home/index.jsx
@@ -1,6 +1,6 @@
 import ScrollToTop from "@/src/hooks/scroll-to-top";
 import Footer from "@/src/layout/footers/footer";
-import Header from "@/src/layout/headers/header";
+import Header from "@/src/layout/headers/header-3";
 import React from "react";
 import AboutArea from "../../../common/about-area";
 import FeatureArea from "./feature-area";
@@ -14,7 +14,7 @@ import TestimonialArea from "./testimonial-area";
 const HomeOne = () => {
   return (
     <>
-      <Header />
+   
       <div id="smooth-wrapper">
       <div id="smooth-content">
       <main className="fix">
